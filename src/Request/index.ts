d6ef997b2831add9a4ee91b95fd62629731fe439
Francisco Blanco <blancofrancisco34@gmail.com>
2023-09-19 14:49:21 -0500

    onRequest

--- src/Request/index.ts
+++ src/Request/index.ts
@@ -1,14 +1,15 @@
 import axios, { AxiosRequestConfig, AxiosResponse } from "axios";
 
-export type RequestFuntionResutl<R = any> = Promise<Partial<AxiosResponse<R>>>;
+export type RequestFuntionConfig<D = any> = AxiosRequestConfig<D>;
+export type RequestFuntionResult<R = any> = Promise<Partial<AxiosResponse<R>>>;
 
 export type RequestFuntion<D = any, R = any> = (
-    config: AxiosRequestConfig<D>,
-) => RequestFuntionResutl<R>;
+    config: RequestFuntionConfig<D>,
+) => RequestFuntionResult<R>;
 
-export const request = async <D = any, R = any>(
-    config: AxiosRequestConfig<D>,
-): RequestFuntionResutl<R> => {
+export const Request = async <D = any, R = any>(
+    config: RequestFuntionConfig<D>,
+): RequestFuntionResult<R> => {
     try {
         const response = await axios({
             ...config,
--- src/base.ts
+++ src/base.ts
@@ -1,19 +1,30 @@
+import { Request, RequestFuntionConfig, RequestFuntionResult } from "./Request";
+
 export interface TilledConfig {
-    mode:"sandbox"|"production"
+    mode: "sandbox" | "production";
 }
 
 export class TilledBase {
     private config: TilledConfig;
-    private url_sandbox = "https://sandbox-api.tilled.com"
-    private url_production = "https://api.tilled.com"
-    private url = ""
+    private url_sandbox = "https://sandbox-api.tilled.com";
+    private url_production = "https://api.tilled.com";
+    private url = "";
 
     constructor(config: TilledConfig) {
         this.config = config;
-        this.url = config.mode == "sandbox" ? this.url_sandbox : this.url_production
+        this.url =
+            config.mode == "sandbox" ? this.url_sandbox : this.url_production;
     }
 
     public getConfig() {
         return this.config;
     }
+    public onRequest = async <D = any, R = any>(
+        config: RequestFuntionConfig<D>,
+    ): RequestFuntionResult<R> => {
+        return await Request<D, R>({
+            ...config,
+            url: `${this.url}${config.url}`,
+        });
+    };
 }
