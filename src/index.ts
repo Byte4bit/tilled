ef7bcfdc94e51106960809710ed36a752d43de1a
Francisco Blanco <blancofrancisco34@gmail.com>
2023-09-21 13:06:06 -0500

    customer create

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,20 @@
 import { TilledBase } from "./Base";
 import { TilledConfigProps } from "./Config";
 import { TilledPayment } from "./Payment";
+import { TilledCustomer } from "./Customer";
 export class Tilled extends TilledBase {
     public payment: TilledPayment;
+    public customer: TilledCustomer;
 
     constructor(config: TilledConfigProps) {
         super(config);
+        this.customer = new TilledCustomer(this);
         this.payment = new TilledPayment(this);
     }
 }
 
 export const __Tilled = (config: TilledConfigProps) => {
     const c = new Tilled(config);
-    const { payment } = c;
-    return { payment };
+    const { payment,customer } = c;
+    return { payment ,customer};
 };
