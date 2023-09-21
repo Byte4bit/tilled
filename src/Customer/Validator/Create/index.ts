f9c9c08c9fe37d0ff4723ab06a78c1ffc99bae30
Francisco Blanco <blancofrancisco34@gmail.com>
2023-09-21 13:03:46 -0500

    validator customer

--- src/Customer/Validator/Create/index.ts
+++ src/Customer/Validator/Create/index.ts
@@ -1,11 +1,22 @@
-import {
-    onCreateProps,
-} from "../../Interface/Create/props";
+import { onCreateProps } from "../../Interface/Create/props";
 import { FenextjsValidator } from "fenextjs-validator";
 
 export const ValidatorCustomerCreate = FenextjsValidator<onCreateProps>()
     .setName("create-customer")
     .isRequired()
     .isObject({
-        
+        email: FenextjsValidator().isString().isRequired(),
+        first_name: FenextjsValidator()
+            .isString()
+            .isRequired()
+            .isMaxOrEqual(255),
+        last_name: FenextjsValidator()
+            .isString()
+            .isRequired()
+            .isMaxOrEqual(255),
+        middle_name: FenextjsValidator()
+            .isString()
+            .isRequired()
+            .isMaxOrEqual(255),
+        phone: FenextjsValidator().isString().isRequired(),
     });
